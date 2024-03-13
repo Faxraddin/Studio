f43bc39cad1ff93530ea82ebe72854bb1c256f64
Faxraddin <reydivonay@gmail.com>
2024-03-13 22:39:06 +0400

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import GlobalStyles from './GlobalStyles';
+import "@fontsource/kaushan-script";
+import "@fontsource/sirin-stencil";
 
 function App() {
 
   return (
     <>
       <GlobalStyles/>
-
+      <h1>bunjkml</h1>
     </>
   )
 }
