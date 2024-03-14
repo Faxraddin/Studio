9f26a75317952737354c387bed13e28e61112d07
Faxraddin <reydivonay@gmail.com>
2024-03-14 22:02:58 +0400

    themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 import GlobalStyles from './GlobalStyles';
 import "@fontsource/kaushan-script";
 import "@fontsource/sirin-stencil";
+import {dark} from './Themes'
 
 function App() {
 
   return (
     <>
       <GlobalStyles/>
-      <h1>bunjkml</h1>
+      <ThemeProvider theme={dark}>
+        <h1>bunjkml</h1>
+      </ThemeProvider>
     </>
   )
 }
---
+++ src/Themes.jsx
@@ -0,0 +1,19 @@
+export const dark = {
+    body:'#202020',
+    bodyRgba:'32,32, 32',
+    text:'#fff',
+    textRgba:'255,255,255',
+
+    grey:'#bebebe',
+
+    fontxs:'0.75em',
+    fontsm:'0.75em',
+    fontmd:'0.75em',
+    fontlg:'0.75em',
+    fontxl:'0.75em',
+    fontxxl:'0.75em',
+    fontxxxl:'0.75em',
+    fontBig:'0.75em',
+    
+    navHeight:'5rem'
+}
