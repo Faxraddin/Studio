b5d0fbe8d484e2bab463e90167167efb694a4a40
Faxraddin <reydivonay@gmail.com>
2024-03-14 22:09:18 +0400

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { ThemeProvider } from 'styled-components';
 import './App.css';
+import { useRef } from 'react';
 import GlobalStyles from './GlobalStyles';
 import "@fontsource/kaushan-script";
 import "@fontsource/sirin-stencil";
-import {dark} from './Themes'
+import {dark} from './Themes';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
 
 function App() {
+
+  const containerRef = useRef(null);
 
   return (
     <>
       <GlobalStyles/>
       <ThemeProvider theme={dark}>
-        <h1>bunjkml</h1>
+        <LocomotiveScrollProvider
+          options={{smooth:true}}
+          watch={[]}
+          containerRef={containerRef}
+        >
+          <main data-scroll-container ref={containerRef} className='app'>
+            <h1>hnjk</h1>
+          </main>
+        </LocomotiveScrollProvider>
       </ThemeProvider>
     </>
   )
