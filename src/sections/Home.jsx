1afb25956e7fa48271d103446fd8f632fb459136
Faxraddin <reydivonay@gmail.com>
2024-03-15 20:51:58 +0400

    logo

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import CoverVideo from "../components/CoverVideo";
+import Logo from "../components/Logo";
 
 const Section = styled.section`
 position:relative;
@@ -12,6 +13,7 @@ const Home = () => {
   return (
     <Section>
         <CoverVideo/>
+        <Logo/>
     </Section>
   )
 }
