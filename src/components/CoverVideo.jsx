7e60b4ec369d7d3c254fdfe2e93999a0f659799b
Faxraddin <reydivonay@gmail.com>
2024-03-15 12:46:20 +0400

    video

--- src/components/CoverVideo.jsx
+++ src/components/CoverVideo.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import MainVideo from "../assets/Walking Girl.mp4"
 
 const VideoContainer = styled.section`
 height:100vh;
 width:100%;
-position:relative
+position:relative,
+
+video{
+    height:100vh;
+    width:100%;
+    object-fit:cover
+}
+`
+
+const DarkOverlayed = styled.div`
+position:absolute;
+top:0;
+left:0;
+bottom:0;
+right:0;
+z-index:1;
+
+background-color:${props=>`rgba(${props.theme.bodyRgba},0.6)`};
 `
 
 const CoverVideo = () => {
   return (
     <VideoContainer>
-        <video src="">
-
-        </video>
+        <DarkOverlayed/>
+        <video src={MainVideo} loop muted type='video/mp4' autoPlay/>
     </VideoContainer>
   )
 }
