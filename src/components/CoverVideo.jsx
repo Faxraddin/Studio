1e45dff4c4b65547362abb919f13d39e5f312c4f
Faxraddin <reydivonay@gmail.com>
2024-03-15 20:24:21 +0400

    video done

--- src/components/CoverVideo.jsx
+++ src/components/CoverVideo.jsx
@@ -34,6 +34,7 @@ right:0;
 z-index:5;
 
 display:flex;
+flex-direction:column;
 justify-content:center;
 align-items:center;
 color: ${props => props.theme.text}
@@ -45,9 +46,13 @@ const CoverVideo = () => {
     <VideoContainer>
         <DarkOverlayed/>
         <Title>
-            <div>
-                <h1 className="text-[10em] font-[kaushan-script]">Wibe</h1>
+            <div className="flex">
+                <h1 data-scroll data-scroll-speed='4' data-scroll-delay='0.13' className="text-[10em]" style={{font:'kaushan-script shadow-[1px 1px 1px 1px]'}}>W</h1>
+                <h1 data-scroll data-scroll-speed='4' data-scroll-delay='0.9' className="text-[10em]" style={{font:'kaushan-script shadow-[1px 1px 1px 1px]'}}>i</h1>
+                <h1 data-scroll data-scroll-speed='4' data-scroll-delay='0.6' className="text-[10em]" style={{font:'kaushan-script shadow-[1px 1px 1px 1px]'}}>b</h1>
+                <h1 data-scroll data-scroll-speed='4' data-scroll-delay='0.4' className="text-[10em]" style={{font:'kaushan-script shadow-[1px 1px 1px 1px]'}}>e</h1>
             </div>
+            <h2 data-scroll data-scroll-speed='2' data-scroll-delay='0.4' className="text-[1.25em]" style={{font:'kaushan-script shadow-[1px 1px 1px 1px]'}}>Inspire. Create. Belive</h2>
         </Title>
         <video src={MainVideo} loop muted type='video/mp4' autoPlay/>
     </VideoContainer>
