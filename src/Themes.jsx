9e96b1bef83df1cbf7a6fe1545566e978d12c5aa
Faxraddin <reydivonay@gmail.com>
2024-03-15 12:58:08 +0400

    title

--- src/Themes.jsx
+++ src/Themes.jsx
@@ -7,13 +7,13 @@ export const dark = {
     grey:'#bebebe',
 
     fontxs:'0.75em',
-    fontsm:'0.75em',
-    fontmd:'0.75em',
-    fontlg:'0.75em',
-    fontxl:'0.75em',
-    fontxxl:'0.75em',
-    fontxxxl:'0.75em',
-    fontBig:'0.75em',
+    fontsm:'0.875em',
+    fontmd:'1em',
+    fontlg:'1.25em',
+    fontxl:'2em',
+    fontxxl:'3em',
+    fontxxxl:'5em',
+    fontBig:'10em',
     
     navHeight:'5rem'
 }
--- src/components/CoverVideo.jsx
+++ src/components/CoverVideo.jsx
@@ -25,10 +25,30 @@ z-index:1;
 background-color:${props=>`rgba(${props.theme.bodyRgba},0.6)`};
 `
 
+const Title = styled.div`
+position:absolute;
+top:0;
+left:0;
+bottom:0;
+right:0;
+z-index:5;
+
+display:flex;
+justify-content:center;
+align-items:center;
+color: ${props => props.theme.text}
+}
+`
+
 const CoverVideo = () => {
   return (
     <VideoContainer>
         <DarkOverlayed/>
+        <Title>
+            <div>
+                <h1 className="text-[10em] font-[kaushan-script]">Wibe</h1>
+            </div>
+        </Title>
         <video src={MainVideo} loop muted type='video/mp4' autoPlay/>
     </VideoContainer>
   )
