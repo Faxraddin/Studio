f4406b80bd8811a129a7432642b14c2349da2ba8
Faxraddin <reydivonay@gmail.com>
2024-03-16 11:12:59 +0400

    navbar almost done

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const [click,setClick] = useState(false)
   return (
-    <div>Navbar</div>
+    <div className="w-[100vw] z-[6] absolute top-0 flex justify-center items-center">
+      <ul className={`relative px-[10rem] py-0 ${click ? 'h-[5rem]' : 'h-0 '} text-[#fff] w-full flex justify-around items-center bg-[#202020]`}>
+        <li onClick={()=>{setClick(!click)}} style={{listStyle:'none'}} className="absolute left-1/2 -translate-x-1/2 top-full text-[#202020] text-[1em] font-bold polygon bg-[#ffff]/75 flex justify-center items-center w-[15rem] cursor-pointer h-[2.5rem]">MENU</li>
+
+        <li className="uppercase text-[#fff]">Home</li>
+        <li className="uppercase text-[#fff]">About</li>
+        <li className="uppercase text-[#fff]">Shop</li>
+        <li className="uppercase text-[#fff]">New Arrival</li>
+      </ul>
+    </div>
   )
 }
 export default Navbar
