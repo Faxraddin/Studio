1e803d4455e4b5ae4a111c47e499e2fb282b67af
Faxraddin <reydivonay@gmail.com>
2024-03-16 11:39:29 +0400

    done

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -4,16 +4,16 @@ import { motion } from "framer-motion";
 const Navbar = () => {
   const [click,setClick] = useState(false)
   return (
-    <div className="w-[100vw] z-[6] absolute top-0 flex justify-center items-center">
-      <ul className={`relative px-[10rem] py-0 ${click ? 'h-[5rem]' : 'h-0 '} text-[#fff] w-full flex justify-around items-center bg-[#202020]`}>
+    <motion.div initial={{y:'-5rem'}} animate={{y:0}} transition={{duration:1.5,delay:2}} className={`w-[100vw] z-[6] absolute top-0 flex justify-center items-center`}>
+      <ul className={`relative px-[10rem] py-0 ${click ? 'h-[5rem]' : 'h-0 '} cursor-pointer transition-all duration-300 ease-in text-[#fff] w-full flex bg-[#202020] justify-around items-center`}>
         <li onClick={()=>{setClick(!click)}} style={{listStyle:'none'}} className="absolute left-1/2 -translate-x-1/2 top-full text-[#202020] text-[1em] font-bold polygon bg-[#ffff]/75 flex justify-center items-center w-[15rem] cursor-pointer h-[2.5rem]">MENU</li>
 
-        <li className="uppercase text-[#fff]">Home</li>
-        <li className="uppercase text-[#fff]">About</li>
-        <li className="uppercase text-[#fff]">Shop</li>
-        <li className="uppercase text-[#fff]">New Arrival</li>
+        <motion.li transition={{duration:0}} whileHover={{scale:1.1,y:-5}} whileTap={{scale:0.8, y:0}} className={`uppercase text-[#fff] transition-all duration-300 ease-in ${click ? 'opacity-1':'opacity-0'}`}>Home</motion.li>
+        <motion.li transition={{duration:0}} whileHover={{scale:1.1,y:-5}} whileTap={{scale:0.8, y:0}} className={`uppercase text-[#fff] transition-all duration-300 ease-in ${click ? 'opacity-1':'opacity-0'}`}>About</motion.li>
+        <motion.li transition={{duration:0}} whileHover={{scale:1.1,y:-5}} whileTap={{scale:0.8, y:0}} className={`uppercase text-[#fff] transition-all duration-300 ease-in ${click ? 'opacity-1':'opacity-0'}`}>Shop</motion.li>
+        <motion.li transition={{duration:0}} whileHover={{scale:1.1,y:-5}} whileTap={{scale:0.8, y:0}} className={`uppercase text-[#fff] transition-all duration-300 ease-in ${click ? 'opacity-1':'opacity-0'}`}>New Arrival</motion.li>
       </ul>
-    </div>
+    </motion.div>
   )
 }
 export default Navbar
