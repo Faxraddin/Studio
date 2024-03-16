3496c35975421799c8dbd373176ff234845d143d
Faxraddin <reydivonay@gmail.com>
2024-03-16 10:36:15 +0400

    navbar

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import CoverVideo from "../components/CoverVideo";
 import Logo from "../components/Logo";
+import Navbar from "../layouts/Navbar";
 
 const Section = styled.section`
 position:relative;
@@ -14,7 +15,7 @@ const Home = () => {
     <Section>
         <CoverVideo/>
         <Logo/>
-        
+        <Navbar/>
     </Section>
   )
 }
