f73f369299538ae076a8b292c5d5fbf4c90972f1
Faxraddin <reydivonay@gmail.com>
2024-03-19 13:35:03 +0400

    shop p1

--- src/sections/Shop.jsx
+++ src/sections/Shop.jsx
@@ -7,16 +7,39 @@ import img3 from '../assets/Images/3.webp'
 
 const Section = styled.section`
 position:relative;
-min-height: 100vh;
-width:80vw;
+width:100vw;
+height:auto;
+overflow:hidden;
 
-dislay:flex;
+display: flex; 
+justify-content:flex-start;
+align-items:flex-start;
 margin:0 auto;
 `
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <Section data-scroll data-scroll-speed='-1'>
+      <h1 style={{fontFamily:'Kaushan Script'}} className="text-[5em] shadow-lg z-[11] text-[#fff] absolute m-auto font-semibold top-[1rem] left-[5%]">New Arrivals</h1>
+      <div className="bg-[#202020] text-[#fff] z-[5]  left-0 flex justify-center items-center w-[35%] min-h-[100vh]">
+        <p className="text-[1.25em] font-semibold w-4/5 m-[0 auto]">
+          The brand new collection is currently being developed in USA. 
+          We create our products using best quality material, including the use of some of the pure fabrics to make our products. 
+          All products are made using the best materials, from the finest cotton to the finest fabrics.
+
+          We have lots of different clothing options like shoes, jackets and dresses. 
+          Not only clothes but we also provide unique Jewellery as well. 
+          It is great for us to carry our new clothes all around the country and look different.
+        </p>
+      </div>
+      <div className="bg-[#bebebe] absolute left-[35%] min-h-[100vh] w-[65%] flex justify-start items-center pl-[30%]">
+        <h1 className="w-[5rem] margin-[0 2rem]">img</h1>
+        <h1 className="w-[5rem] margin-[0 2rem]">img</h1>
+        <h1 className="w-[5rem] margin-[0 2rem]">img</h1>
+        <h1 className="w-[5rem] margin-[0 2rem]">img</h1>
+        <h1 className="w-[5rem] margin-[0 2rem]">img</h1>
+      </div>
+    </Section>
   )
 }
 export default Shop
